3f2bbec9b0461688a0a6b205f7dbeaffa45febc2
MingjueYu <55510131+MingjueYu@users.noreply.github.com>
2019-09-23 08:54:22 -0300

    Create part2.py

---
+++ part2.py
@@ -0,0 +1,21 @@
+#1
+def rotate_word(word):
+  index = 0
+  pair = 0
+  while index < len
+  
+#2
+def duplicate_words(file):
+  file = open("2600-0.txt")
+  for line in file:
+    word = line.strip()
+    new_list = list(word)
+  for ele in new_list:
+    index = 0
+    if ele[index] == ele[index+1]
+      return True
+      index = index + 1
+  returen False
+
+
+  
